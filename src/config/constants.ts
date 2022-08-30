4910405406d360eb001cf90f97e3d74b6a7dcce3
90sRehem <you@example.com>
2022-08-30 19:46:48 -0300

    feat: add jwt package + config updates

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,7 @@
+import "dotenv/config";
+
 export const APP_PORT = process.env.PORT || 3333;
+export const ACCESS_TOKEN_PRIVATE_KEY = process.env
+  .ACCESS_TOKEN_PRIVATE_KEY as string;
+export const ACCESS_TOKEN_PUBLIC_KEY = process.env
+  .ACCESS_TOKEN_PUBLIC_KEY as string;
